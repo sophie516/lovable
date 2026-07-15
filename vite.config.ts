b5904c817bbebffb3fc79c009dc4a9d8903390cd
sophie516 <sophie@schumacher.plus>
2026-07-15 16:44:03 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: './', // This tells Vite: "Look for index.html right here!"
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
